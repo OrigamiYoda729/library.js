a496cd6b19cc979705de2ea18f6f391d0d3ff862
OrigamiYoda729 <33796301+OrigamiYoda729@users.noreply.github.com>
2018-03-12 22:22:27 -0400

    Minified slim/library.js, Added more CDN Support

--- testbuild/commit.bat
+++ testbuild/commit.bat
@@ -1,25 +1,28 @@
 @echo off
 set /p update="Enter Changes: "
 cls
-@echo Processing 1/7 [=------]
+@echo Processing 1/8 [=-------]
+cd ../
+cls
+@echo Processing 2/8 [==------]
 git config --global user.email 33796301+OrigamiYoda729@users.noreply.github.com
 cls
-@echo Processing 2/7 [==-----]
+@echo Processing 3/8 [===-----]
 git config --global user.name OrigamiYoda729
 cls
-@echo Processing 3/7 [===----]
+@echo Processing 4/8 [====----]
 git init
 cls
-@echo Processing 4/7 [====---]
+@echo Processing 5/8 [=====---]
 git pull https://github.com/OrigamiYoda729/library.js.git master
 cls
-@echo Processing 5/7 [=====--]
+@echo Processing 6/8 [======--]
 git add --all
 cls
-@echo Processing 6/7 [======-]
-git commit -m "Update: %update%"
+@echo Processing 7/8 [=======-]
+git commit -m "%update%"
 cls
-@echo Processing 7/7 [=======]
+@echo Processing 8/8 [========]
 git push https://github.com/OrigamiYoda729/library.js.git master
 cls
 @echo Complete! Press Enter to Exit
--- testbuild/slim/library.js
+++ testbuild/slim/library.js
@@ -13,7 +13,7 @@ library = {
 			if (plugin != "invalid") {
 				library.debug.remove(plugin);
 			} else {
-				console.log('"' + cplugin + '"' + ' is an invalid plugin.');
+				console.log('"' + cplugin + '"' + ' is an invalid library.');
 			}
 		}   
 	},
@@ -204,7 +204,6 @@ library = {
 		remove: function(rsource) {
 			var crsource = rsource.split(".");
 			var rfiletype = crsource[crsource.length - 1];
-			
 			if (rfiletype == "css") {
 				var rcur_stylesheet = document.getElementsByTagName("link");
 				for (i = 0; i < rcur_stylesheet.length; i++) {
@@ -217,14 +216,14 @@ library = {
 			if (rfiletype == "js" || rfiletype == "javascript") {
 				var rcur_script = document.getElementsByTagName("script");
 				for (i = 0; i < rcur_script.length; i++) {
-					if (rcur_script[i].href == rsource) {
+					if (rcur_script[i].src == rsource) {
 						document.getElementsByTagName("script")[i].removeAttribute("src");
 					}
 				}
 			} else {
 				var rcur_cscript = document.getElementsByTagName("script");
 				for (i = 0; i < rcur_cscript.length; i++) {
-					if (rcur_cscript[i].href == rsource) {
+					if (rcur_cscript[i].src == rsource) {
 						document.getElementsByTagName("script")[i].removeAttribute("src");
 						document.getElementsByTagName("script")[i].removeAttribute("type");
 					}
@@ -233,11 +232,92 @@ library = {
 		},
 		cdn: function(ck_plugin) {
 
+			if (ck_plugin === "jquery") {
+				return "https://code.jquery.com/jquery-3.3.1.min.js";
+			} else 
+			if (ck_plugin === "jquery-slim") {
+				return "https://code.jquery.com/jquery-3.3.1.slim.min.js";
+			} else
+			if (ck_plugin === "require-js") {
+				return "https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.5/require.min.js";
+			} else
+			if (ck_plugin === "popper-js") {
+				return "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js";
+			} else
 			if (ck_plugin === "bootstrap-css") {
 				return "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css";
-			} else 
+			} else
 			if (ck_plugin === "bootstrap-js") {
 				return "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js";
+			} else 
+			if (ck_plugin === "bootstrap-bundle") {
+				return "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js";
+			} else 
+			if (ck_plugin === "bootswatch-cerulean") {
+				return "https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/cerulean/bootstrap.min.css";
+			} else 
+			if (ck_plugin === "bootswatch-cosmo") {
+				return "https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/cerulean/bootstrap.min.css";
+			} else 
+			if (ck_plugin === "bootswatch-cyborg") {
+				return "https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/cyborg/bootstrap.min.css";
+			} else 
+			if (ck_plugin === "bootswatch-darkly") {
+				return "https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/darkly/bootstrap.min.css";
+			} else 
+			if (ck_plugin === "bootswatch-flatly") {
+				return "https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/flatly/bootstrap.min.css";
+			} else 
+			if (ck_plugin === "bootswatch-journal") {
+				return "https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/journal/bootstrap.min.css";
+			} else 
+			if (ck_plugin === "bootswatch-litera") {
+				return "https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/litera/bootstrap.min.css";
+			} else 
+			if (ck_plugin === "bootswatch-lux") {
+				return "https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/lux/bootstrap.min.css";
+			} else 
+			if (ck_plugin === "bootswatch-materia") {
+				return "https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/materia/bootstrap.min.css";
+			} else 
+			if (ck_plugin === "bootswatch-minty") {
+				return "https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/minty/bootstrap.min.css";
+			} else 
+			if (ck_plugin === "bootswatch-pulse") {
+				return "https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/pulse/bootstrap.min.css";
+			} else 
+			if (ck_plugin === "bootswatch-sandstone") {
+				return "https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/sandstone/bootstrap.min.css";
+			} else 
+			if (ck_plugin === "bootswatch-sketchy") {
+				return "https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/sketchy/bootstrap.min.css";
+			} else 
+			if (ck_plugin === "bootswatch-simplex") {
+				return "https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/simplex/bootstrap.min.css";
+			} else 
+			if (ck_plugin === "bootswatch-slate") {
+				return "https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/slate/bootstrap.min.css";
+			} else 
+			if (ck_plugin === "bootswatch-solar") {
+				return "https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/solar/bootstrap.min.css";
+			} else 
+			if (ck_plugin === "bootswatch-spacelab") {
+				return "https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/spacelab/bootstrap.min.css";
+			} else 
+			if (ck_plugin === "bootswatch-superhero") {
+				return "https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/superhero/bootstrap.min.css";
+			} else 
+			if (ck_plugin === "bootswatch-united") {
+				return "https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/united/bootstrap.min.css";
+			} else 
+			if (ck_plugin === "bootswatch-yeti") {
+				return "https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0/yeti/bootstrap.min.css";
+			} else 
+			if (ck_plugin === "font-awesome") {
+				return "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
+			} else
+			if (ck_plugin === "ionicons") {
+				return "http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css";
 			} else {
 				return "invalid";
 			}
